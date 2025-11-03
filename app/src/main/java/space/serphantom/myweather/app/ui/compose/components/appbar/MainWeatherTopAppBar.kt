5125d7531e30e041ff2bfb45a943a04f85cb1254
Sergey Lebedev <sergey.lebedev@sequenia.com>
2025-11-03 23:41:55 +0700

    Добавлен Data class содержащий все необходимые данные для отображения погоды в toolbar.

--- app/src/main/java/space/serphantom/myweather/app/ui/compose/components/appbar/MainWeatherTopAppBar.kt
+++ app/src/main/java/space/serphantom/myweather/app/ui/compose/components/appbar/MainWeatherTopAppBar.kt
@@ -35,27 +35,6 @@ import space.serphantom.myweather.app.ui.compose.extensions.blockAllGestures
 import space.serphantom.myweather.app.ui.compose.theme.AppTheme
 
 /**
- * Data class содержащий все необходимые данные для отображения погоды в toolbar
- * @property cityName Название города
- * @property disclaimer Описание погодных условий
- * @property temperature Текущая температура
- * @property minTemperature Минимальная температура
- * @property maxTemperature Максимальная температура
- * @property unitMeasurement Единица измерения температуры
- * @property weatherIconUrl URL иконки погоды
- */
-@Immutable
-data class MainWeatherTopAppBarData(
-    val cityName: String,
-    val disclaimer: String?,
-    val temperature: String,
-    val minTemperature: String,
-    val maxTemperature: String,
-    val unitMeasurement: String,
-    val weatherIconUrl: String?,
-)
-
-/**
  * Главный компонент верхней панели приложения погоды с поддержкой скролла и эффектом размытия
  *
  * @param [scrollBehavior] Поведение скролла для синхронизации с контентом
